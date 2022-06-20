b03ab2364cdc1678b954af8addc16e00eed12c43
Won Kyun Park <wonkyunpark@Wons-MacBook-Pro.local>
2022-06-20 21:07:11 +0900

    delete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //express basic step
 const express = require("express");
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
 const request = require("request");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
@@ -14,49 +14,49 @@ app.use(express.static("public"));
 
 mongoose.connect('mongodb://127.0.0.1:27017/wikiDB', {useNewUrlParser: true});
 
-const articleSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
+const articleSchema = new mongoose.Schema({title: String, content: String});
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/", (req,res) => {
-  res.redirect("/articles");
+app.get("/", (req, res) => {
+    res.redirect("/articles");
 })
 
-
-app.get("/articles", (req,res)=>{
-  Article.find({}, (error,foundArticle) => {
-    if(!error){
-      res.render("main", {article: foundArticle});
-    }else {
-      res.send(error);
-    }
-  })
+app.get("/articles", (req, res) => {
+    Article.find({}, (error, foundArticle) => {
+        if (!error) {
+           res.send(foundArticle)
+        } else {
+            res.send(error);
+        }
+    })
 })
 app.post("/articles", (req, res) => {
-  console.log(req.body.title);
-  console.log(req.body.content);
+    console.log(req.body.title);
+    console.log(req.body.content);
 
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
-  });
-  newArticle.save((err)=>{
-    if(err){
-      res.send(err);
-    }else {
-      res.send("Successfully added a new article.");
-    }
-  });
-  
+    const newArticle = new Article(
+        {title: req.body.title, content: req.body.content}
+    );
+    newArticle.save((err) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("Successfully added a new article.");
+        }
+    });
 
 })
-
-
-
-app.listen(3000, ()=>{
-  console.log("server is running on port 3000");
+app.delete("/articles", (req, res) => {
+    Article.deleteMany((err) => {
+        if (err) {
+            err.send(err);
+        } else {
+            res.send("All deleted");
+        }
+    })
 })
 
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+})
